f720f329e2de301e4c9a0e642d3ddd5e6f7c5668
mmmmm1233212344 <abhaymilan8@gmail.com>
2022-10-06 11:48:21 +0530

    Changed App.js

--- src/App.js
+++ src/App.js
@@ -10,11 +10,12 @@ function App(){
       placeholder="Search.." 
       onChange={event => {setSearchTerm(event.target.value)}}></input>
      {JSONDATA.filter((val)=>{
-       if (searchTerm == ''){
+       if (searchTerm === ''){
          return val
        } else if (val.first_name.toLowerCase().includes(searchTerm.toLowerCase())){
          return val
        }
+       return null
      })
      .map ((val,key)=>{
        return <div className="user" key={key}>{val.first_name} </div>
